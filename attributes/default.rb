3f3151b5e23d542b39f532d20a6bb7514e1df8cf
anshulpatel25 <anshulpatel25@gmail.com>
2015-12-16 20:02:02 +0530

    Commit Initial Dev

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,9 +3,10 @@
 #attributes : default
 #
 
-default[:aws_vpc][:region] = 'us-west-2'
-default[:aws_vpc][:vpc] = ''
-default[:aws_vpc][:vpc_cidr] = ''
-default[:aws_vpc][:public_subnet] = []
-default[:aws_vpc][:private_subnet] = []
+default[:aws_vpc][:region] = 'aws::us-west-2'
+default[:aws_vpc][:vpc] = 'final_vpc_cookbook'
+default[:aws_vpc][:vpc_cidr] = '10.0.0.0/24'
+default[:aws_vpc][:public_subnet] = 'final_vpc_subnet_cookbook'
+default[:aws_vpc][:avail_zone] = 'us-west-2b'
+default[:aws_vpc][:subnet_cidr] = '10.0.0.0/26'
 
--- recipes/vpc_simple.rb
+++ recipes/vpc_simple.rb
@@ -1,11 +1,6 @@
-#
-#Chef: Cookbook : aws_vpc
-#Recipe : vpc_pub
-#
-
 require 'chef/provisioning/aws_driver'
 
-with_driver 'aws::#{node[:aws_vpc][:region]}'
+with_driver node[:aws_vpc][:region]
   aws_vpc node[:aws_vpc][:vpc] do
     cidr_block node[:aws_vpc][:vpc_cidr]
     internet_gateway true
@@ -13,12 +8,11 @@ with_driver 'aws::#{node[:aws_vpc][:region]}'
   end
 
 
-node[:aws_vpc][:public_subnet].each do |name,cidr|  
-   puts("Inside the loop")
-   aws_subnet name do
+
+  aws_subnet node[:aws_vpc][:public_subnet] do
    vpc node[:aws_vpc][:vpc]
-   cidr_block cidr
-   availability_zone node[:aws_vpc][:region]
+   cidr_block node[:aws_vpc][:subnet_cidr] 
+   availability_zone node[:aws_vpc][:avail_zone]
    map_public_ip_on_launch true
   end
 
